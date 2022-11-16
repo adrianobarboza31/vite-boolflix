2ada2399c2582748a8c188279d0dfe488866b066
adrianobarboza31 <barbozaa022@gmail.com>
2022-11-16 18:14:48 +0100

    add search bar+button  e

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HeaderComponent/>
+  <main>
+  <Card/>
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+import Card from './components/Card.vue'
+import HeaderComponent from './components/Header.vue'
+  export default {
+    data(){
+      return{
+        
+      }
+    },
+    components:{
+      Card,
+      HeaderComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <div>
+            <h1>boolflix</h1>
+        </div>
+        <div class="d-flex row ">
+            <input type="text">
+            <button>Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
